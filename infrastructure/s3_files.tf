7d2080f040078a00c983c7904505003f678d857f
edilson-lobo <edilsonmlobo21@gmail.com>
2022-05-06 08:18:52 -0300

    Correcao bug no arquivo s3_files.tf

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -16,9 +16,9 @@ resource "aws_s3_bucket_object" "delta_insert" {
 
 resource "aws_s3_bucket_object" "delta_upsert" {
   bucket = aws_s3_bucket.dl.id
-  key = "emr-code/pyspark/01_delta_spark_upsert.py"
+  key = "emr-code/pyspark/02_delta_spark_upsert.py"
   acl = "private"
-  source = "../etl/01_delta_spark_upsert.py"
-  etag = filemd5("../etl/01_delta_spark_upsert.py")
+  source = "../etl/02_delta_spark_upsert.py"
+  etag = filemd5("../etl/02_delta_spark_upsert.py")
 }
 
